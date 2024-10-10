8733f2bfd1764fcc3167a4627dbdffa66d219145
Jose Alejandro Moreno <166447988+JochiKum@users.noreply.github.com>
2024-10-10 11:10:28 -0400

    wip: models (#3)

--- src/api/admin.py
+++ src/api/admin.py
@@ -1,11 +1,12 @@
 import os
 from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
-from .models import db, Users
+from .models import db, Users, TrainingPlans, TrainingSessions
 
 def setup_admin(app):
     app.secret_key = os.environ.get('FLASK_APP_KEY', 'sample key')
     app.config['FLASK_ADMIN_SWATCH'] = 'darkly'
     admin = Admin(app, name='4Geeks Admin', template_mode='bootstrap3')
     admin.add_view(ModelView(Users, db.session))  # You can duplicate that line to add mew models
-    # admin.add_view(ModelView(YourModelName, db.session))
+    admin.add_view(ModelView(TrainingPlans, db.session))
+    admin.add_view(ModelView(TrainingSessions, db.session))
--- src/api/models.py
+++ src/api/models.py
@@ -1,4 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 
 db = SQLAlchemy()
@@ -9,6 +10,12 @@ class Users(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    name = db.Column(db.String(80), unique=False, nullable=False)
+    age = db.Column(db.String(80), unique=False, nullable=False)
+    weight = db.Column(db.String(80), unique=False, nullable=False)
+    height = db.Column(db.String(80), unique=False, nullable=False)
+    target_weight = db.Column(db.String(80), unique=False, nullable=False)
+    registration_date = db.Column(db.DateTime(), default= False, nullable=False)
 
     def __repr__(self):
         return f'<User {self.id} - {self.email}>'
@@ -17,3 +24,27 @@ class Users(db.Model):
         return {'id': self.id,
                 'email': self.email,
                 'is_active': self.is_active}
+
+
+class TrainingPlans(db.Model):
+    __tablename__ = 'training_plans'
+    id = db.Column(db.Integer, primary_key=True)   
+    name = db.Column(db.String(80), unique=False, nullable=False)
+    duration_minutes = db.Column(db.Integer, nullable=False)
+    burn_calories = db.Column(db.Integer, nullable=False) 
+    level = db.Column(db.Enum('begginer', 'intermediate','advanced', name='level'), nullable=False)  
+    registration_date = db.Column(db.DateTime(), default=datetime.utcnow, nullable=False) 
+    finalization_date = db.Column(db.DateTime(), nullable=False)
+    quantity_session = db.Column(db.Integer, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    user_to = db.relationship('Users', foreign_keys=[user_id], backref=db.backref('training_plans', lazy='select'))
+
+
+class TrainingSessions(db.Model):
+    __tablename__ = 'training_sessions'
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.DateTime(), default=False, nullable=False)
+    duration_minutes = db.Column(db.Integer, nullable=False)
+    training_plan_id = db.Column(db.Integer, db.ForeignKey('training_plans.id'), nullable=False)
+    training_plan_to = db.relationship('TrainingPlans', foreign_keys=[training_plan_id], backref=db.backref('training_sessions', lazy='select'))
+
